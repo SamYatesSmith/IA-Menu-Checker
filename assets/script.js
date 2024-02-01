f5afc827881ae81f8f0d009b8faa7e79b58c6654
SamYatesSmith <>
2024-02-01 14:22:22 +0000

    2.5 Accessing of .values returning unCaughtTE.Researched how to fix.  Found DOMContentLoaded event to ensure script runs after DOM loads. Adjusted code to suit.  Added error messages to console if user does not checkboxes.

--- assets/script.js
+++ assets/script.js
@@ -1,247 +1,261 @@
-let userDiet;
+document.addEventListener('DOMContentLoaded', function() {
 
-function checkChecker() {
-    userDiet = document.querySelector('input[name="diet"]:checked').value;
-    const userIntolernce = array.from(document.querySelectorAll('input[name="intolerance]:checked')).map(checkbox => checkbox.value);
+    let userDiet;
 
-    const menu = [
-        {
-            name: "Artichoke, Tapenade & Olive Sandwich",
-            ingredients: ["sesame", "gluten"],
-            vegetarianFriendly: true,
-            veganFriendly: true
-        },
+    function checkChecker() {
+    userDiet = document.querySelector('input[name="diet"]:checked');
+    const userIntolernce = Array.from(document.querySelectorAll('input[name="intolerance]:checked')).map(checkbox => checkbox.value);
 
-        {
-            name: "Avo, Olives & Toms Sandwich",
-            ingredients: ["sesame", "gluten", "pineNuts"],
-            vegetarianFriendly: true,
-            veganFriendly: true
-        },
+        if (userDiet) {
+            userDiet = userDiet.value;
 
-        {
-            name: "Brie, Tomato & Basil Sandwich",
-            ingredients: ["gluten", "dairy"],
-            vegetarianFriendly: true,
-            veganFriendly: false
-        },
+        const menu = [
+            {
+                name: "Artichoke, Tapenade & Olive Sandwich",
+                ingredients: ["sesame", "gluten"],
+                vegetarianFriendly: true,
+                veganFriendly: true
+            },
 
-        {
-            name: "Chicken Caesar & Bacon Sandwich",
-            ingredients: ["sesame", "gluten", "egg", "fish", "dairy"],
-            vegetarianFriendly: false,
-            veganFriendly: false
-        },
+            {
+                name: "Avo, Olives & Toms Sandwich",
+                ingredients: ["sesame", "gluten", "pineNuts"],
+                vegetarianFriendly: true,
+                veganFriendly: true
+            },
 
-        {
-            name: "Chicken Salad Baguette Sandwich",
-            ingredients: ["gluten", "egg", "dairy"],
-            vegetarianFriendly: false,
-            veganFriendly: false
-        },
+            {
+                name: "Brie, Tomato & Basil Sandwich",
+                ingredients: ["gluten", "dairy"],
+                vegetarianFriendly: true,
+                veganFriendly: false
+            },
 
-        {
-            name: "Smoked Salmon, Egg Mayo & Avo Sandwich",
-            ingredients: ["sesame", "gluten", "egg", "fish"],
-            vegetarianFriendly: false,
-            veganFriendly: false
-        },
+            {
+                name: "Chicken Caesar & Bacon Sandwich",
+                ingredients: ["sesame", "gluten", "egg", "fish", "dairy"],
+                vegetarianFriendly: false,
+                veganFriendly: false
+            },
 
-        {
-            name: "Italian Prosciutto Sandwich",
-            ingredients: ["sesame", "gluten", "egg", "cow"],
-            vegetarianFriendly: false,
-            veganFriendly: false
-        },
+            {
+                name: "Chicken Salad Baguette Sandwich",
+                ingredients: ["gluten", "egg", "dairy"],
+                vegetarianFriendly: false,
+                veganFriendly: false
+            },
 
-        {
-            name: "Mexican Style Avo & Pepper Sandwich",
-            ingredients: ["sesame", "gluten"],
-            vegetarianFriendly: true,
-            veganFriendly: true
-        },
+            {
+                name: "Smoked Salmon, Egg Mayo & Avo Sandwich",
+                ingredients: ["sesame", "gluten", "egg", "fish"],
+                vegetarianFriendly: false,
+                veganFriendly: false
+            },
 
-        {
-            name: "Posh Cheddar & Pickle Sandwich",
-            ingredients: ["sesame", "gluten", "egg", "cow", "mustard"],
-            vegetarianFriendly: true,
-            veganFriendly: false
-        },
+            {
+                name: "Italian Prosciutto Sandwich",
+                ingredients: ["sesame", "gluten", "egg", "cow"],
+                vegetarianFriendly: false,
+                veganFriendly: false
+            },
 
-        {
-            name: "Free Range Egg Mayo & Smoked Salmon Sandwich",
-            ingredients: ["gluten", "egg", "fish",],
-            vegetarianFriendly: false,
-            veganFriendly: false
-        },
+            {
+                name: "Mexican Style Avo & Pepper Sandwich",
+                ingredients: ["sesame", "gluten"],
+                vegetarianFriendly: true,
+                veganFriendly: true
+            },
 
-        {
-            name: "Spicy Tuna Sandwich",
-            ingredients: ["gluten", "egg", "fish", "cow"],
-            vegetarianFriendly: false,
-            veganFriendly: false
-        },
+            {
+                name: "Posh Cheddar & Pickle Sandwich",
+                ingredients: ["sesame", "gluten", "egg", "cow", "mustard"],
+                vegetarianFriendly: true,
+                veganFriendly: false
+            },
 
-        {
-            name: "Sticky Mushroom Bahn Mi Sandwich",
-            ingredients: ["sesame", "gluten", "soya"],
-            vegetarianFriendly: true,
-            veganFriendly: true
-        },
+            {
+                name: "Free Range Egg Mayo & Smoked Salmon Sandwich",
+                ingredients: ["gluten", "egg", "fish",],
+                vegetarianFriendly: false,
+                veganFriendly: false
+            },
 
-        {
-            name: "Tuna Mayo & Cucumber Sandwich",
-            ingredients: ["gluten", "egg", "fish"],
-            vegetarianFriendly: false,
-            veganFriendly: false
-        },
+            {
+                name: "Spicy Tuna Sandwich",
+                ingredients: ["gluten", "egg", "fish", "cow"],
+                vegetarianFriendly: false,
+                veganFriendly: false
+            },
 
-        {
-            name: "Tuna Nicoise Sandwich",
-            ingredients: ["sesame", "gluten", "egg", "fish", "cow"],
-            vegetarianFriendly: false,
-            veganFriendly: false
-        },
+            {
+                name: "Sticky Mushroom Bahn Mi Sandwich",
+                ingredients: ["sesame", "gluten", "soya"],
+                vegetarianFriendly: true,
+                veganFriendly: true
+            },
 
-        {
-            name: "Wiltshire-Cured Ham & Greve Sandwich",
-            ingredients: ["gluten", "egg", "cow", "mustard"],
-            vegetarianFriendly: false,
-            veganFriendly: false
-        },
+            {
+                name: "Tuna Mayo & Cucumber Sandwich",
+                ingredients: ["gluten", "egg", "fish"],
+                vegetarianFriendly: false,
+                veganFriendly: false
+            },
 
-        {
-            name: "BLT Baugette",
-            ingredients: ["gluten", "egg"],
-            vegetarianFriendly: false,
-            veganFriendly: false
-        },
+            {
+                name: "Tuna Nicoise Sandwich",
+                ingredients: ["sesame", "gluten", "egg", "fish", "cow"],
+                vegetarianFriendly: false,
+                veganFriendly: false
+            },
 
-        {
-            name: "Chicken, Avocado & Basil Baugette",
-            ingredients: ["gluten", "egg", "cow"],
-            vegetarianFriendly: false,
-            veganFriendly: false
-        },
+            {
+                name: "Wiltshire-Cured Ham & Greve Sandwich",
+                ingredients: ["gluten", "egg", "cow", "mustard"],
+                vegetarianFriendly: false,
+                veganFriendly: false
+            },
 
-        {
-            name: "Chicken Salad Baugette",
-            ingredients: ["gluten", "egg", "cow"],
-            vegetarianFriendly: false,
-            veganFriendly: false
-        },
+            {
+                name: "BLT Baugette",
+                ingredients: ["gluten", "egg"],
+                vegetarianFriendly: false,
+                veganFriendly: false
+            },
 
-        {
-            name: "Classic Super Club Baugette",
-            ingredients: ["gluten", "egg"],
-            vegetarianFriendly: false,
-            veganFriendly: false
-        },
+            {
+                name: "Chicken, Avocado & Basil Baugette",
+                ingredients: ["gluten", "egg", "cow"],
+                vegetarianFriendly: false,
+                veganFriendly: false
+            },
 
-        {
-            name: "Crayfish & Rocket Baugette",
-            ingredients: ["gluten", "crustacean", "egg"],
-            vegetarianFriendly: false,
-            veganFriendly: false
-        },
+            {
+                name: "Chicken Salad Baugette",
+                ingredients: ["gluten", "egg", "cow"],
+                vegetarianFriendly: false,
+                veganFriendly: false
+            },
 
-        {
-            name: "Free-Range Egg Mayo Baugette",
-            ingredients: ["gluten", "egg", "mustard"],
-            vegetarianFriendly: true,
-            veganFriendly: false
-        },
+            {
+                name: "Classic Super Club Baugette",
+                ingredients: ["gluten", "egg"],
+                vegetarianFriendly: false,
+                veganFriendly: false
+            },
 
-        {
-            name: "Ham & Cheese Baugette",
-            ingredients: ["gluten", "cow"],
-            vegetarianFriendly: false,
-            veganFriendly: false
-        },
+            {
+                name: "Crayfish & Rocket Baugette",
+                ingredients: ["gluten", "crustacean", "egg"],
+                vegetarianFriendly: false,
+                veganFriendly: false
+            },
 
-        {
-            name: "Humous & Veg Baugette",
-            ingredients: ["sesame", "gluten"],
-            vegetarianFriendly: true,
-            veganFriendly: true
-        },
+            {
+                name: "Free-Range Egg Mayo Baugette",
+                ingredients: ["gluten", "egg", "mustard"],
+                vegetarianFriendly: true,
+                veganFriendly: false
+            },
 
-        {
-            name: "Kids' Cheese Triangles Baugette",
-            ingredients: ["gluten", "cow"],
-            vegetarianFriendly: true,
-            veganFriendly: false
-        },
+            {
+                name: "Ham & Cheese Baugette",
+                ingredients: ["gluten", "cow"],
+                vegetarianFriendly: false,
+                veganFriendly: false
+            },
 
-        {
-            name: "Kids' Chicken & Cucumber Triangles Baugette",
-            ingredients: ["gluten", "egg"],
-            vegetarianFriendly: true,
-            veganFriendly: true
-        },
+            {
+                name: "Humous & Veg Baugette",
+                ingredients: ["sesame", "gluten"],
+                vegetarianFriendly: true,
+                veganFriendly: true
+            },
 
-        {
-            name: "Kids' Ham Triangles Baugette",
-            ingredients: ["gluten", "cow"],
-            vegetarianFriendly: false,
-            veganFriendly: false
-        },
+            {
+                name: "Kids' Cheese Triangles Baugette",
+                ingredients: ["gluten", "cow"],
+                vegetarianFriendly: true,
+                veganFriendly: false
+            },
 
-        {
-            name: "Mature Cheddar & Pickle Baugette",
-            ingredients: ["gluten", "egg", "cow"],
-            vegetarianFriendly: true,
-            veganFriendly: false
-        },
+            {
+                name: "Kids' Chicken & Cucumber Triangles Baugette",
+                ingredients: ["gluten", "egg"],
+                vegetarianFriendly: true,
+                veganFriendly: true
+            },
 
-        {
-            name: "Scottish Smoked Salmon Baugette",
-            ingredients: ["gluten", "fish", "cow"],
-            vegetarianFriendly: false,
-            veganFriendly: false
-        },
+            {
+                name: "Kids' Ham Triangles Baugette",
+                ingredients: ["gluten", "cow"],
+                vegetarianFriendly: false,
+                veganFriendly: false
+            },
 
-        {
-            name: "Super Greens & Reds Baugette",
-            ingredients: ["gluten"],
-            vegetarianFriendly: true,
-            veganFriendly: true
-        },
+            {
+                name: "Mature Cheddar & Pickle Baugette",
+                ingredients: ["gluten", "egg", "cow"],
+                vegetarianFriendly: true,
+                veganFriendly: false
+            },
 
-        {
-            name: "Tuna Mayo & Cucumber Baugette",
-            ingredients: ["gluten", "egg", "fish"],
-            vegetarianFriendly: false,
-            veganFriendly: false
-        },
+            {
+                name: "Scottish Smoked Salmon Baugette",
+                ingredients: ["gluten", "fish", "cow"],
+                vegetarianFriendly: false,
+                veganFriendly: false
+            },
 
-        {
-            name: "The VLT Baugette",
-            ingredients: ["gluten"],
-            vegetarianFriendly: false,
-            veganFriendly: false
-        },
-    ];
+            {
+                name: "Super Greens & Reds Baugette",
+                ingredients: ["gluten"],
+                vegetarianFriendly: true,
+                veganFriendly: true
+            },
 
-    const suitableItems = menu.filter(item => {
-        const isVegetarianFriendly = userDiet === "vegetarian" ? item.vegetarianFriendly : true;
-        const isVeganFriendly = userDiet === "vegan" ? item.veganFriendly : true;
-        const containsIntolerance = item.ingredients.some(ingredients => userIntolernce.includes(ingredients));
+            {
+                name: "Tuna Mayo & Cucumber Baugette",
+                ingredients: ["gluten", "egg", "fish"],
+                vegetarianFriendly: false,
+                veganFriendly: false
+            },
 
-        return !containsIntolerance && isVeganFriendly && isVegetarianFriendly;
-    });
+            {
+                name: "The VLT Baugette",
+                ingredients: ["gluten"],
+                vegetarianFriendly: false,
+                veganFriendly: false
+            },
+        ];
 
-    displayResults(suitableItems);
+        const suitableItems = menu.filter(item => {
+            const isVegetarianFriendly = userDiet === "vegetarian" ? item.vegetarianFriendly : true;
+            const isVeganFriendly = userDiet === "vegan" ? item.veganFriendly : true;
+            const containsIntolerance = item.ingredients.some(ingredients => userIntolernce.includes(ingredients));
+
+            return !containsIntolerance && isVeganFriendly && isVegetarianFriendly;
+        });
+
+        displayResults(suitableItems);
+    } else {
+        console.error("Please select a dietary preference")
+    }
 }
 
 function displayResults(items) {
-    const userResults = document.getElementById("result");
-
+        const userResults = document.getElementById("result");
+        if (userResults) {
+        
         if (items.length > 0) {
-            userResults.innerHTML = "<h2>Your personalised menu based on suitability</h2><ul>" + 
-                                    items.map(item => `<li>${item.name}</li>`).join("") + 
-                                    "</ul>";
+            userResults.innerHTML = "<h2>Your personalised menu based on suitability:</h2><ul>" +
+                items.map(item => `<li>${item.name}</li>`).join("") +
+                "</ul>";
         } else {
             userResults.innerHTML = "<p>I'm sorry, there are no suitable menu items on the menu based on your allergy and intolerance selection</p>";
+        } 
+    } else {
+            console.error("userResult div not found")
         }
-}
+    }
+});
+
